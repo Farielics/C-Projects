39ee0c81298c21e71d971d40c12165956dbd6385
Farielics <faryal@alizai.net>
2024-09-19 20:24:38 -0400

    Add files via upload

---
+++ lab1_falizai4_202/lab1_falizai4_202.c
@@ -0,0 +1,13 @@
+/*
+Faryal Alizai G01364057
+CS 262, Lab section 202
+Lab1: printing hello world with my name
+*/
+#include <stdio.h>
+
+int main() {
+	char name[100] = "Faryal Alizai/*";
+	printf("Hello World!\nMy name is %s\n", name);
+	return 0;
+}
+
